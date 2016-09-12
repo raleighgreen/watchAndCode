5304f261215285b6ddea46eddf1b3a7d4f415fc0
Raleigh Green <raleighgreen@gmail.com>
2016-09-12 12:59:20 -0400

    make a todoList object

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,22 @@
-var todos = ['item 1', 'item 2', 'item 3']
+var todoList = {
+  todos: ['item 1', 'item 2', 'item 3']
+};
 
-function displayTodos() {
-  console.log('My todos:', todos);
-}
-
-function addTodos(todo) {
-  todos.push(todo);
-  displayTodos();
-}
-
-function changeTodo(position, newValue) {
-  todos[position] = newValue;
-  displayTodos();
-}
-
-function deleteTodo(position) {
-  todos.splice(position, 1);
-  displayTodos();
-}
+// function displayTodos() {
+//   console.log('My todos:', todos);
+// }
+//
+// function addTodos(todo) {
+//   todos.push(todo);
+//   displayTodos();
+// }
+//
+// function changeTodo(position, newValue) {
+//   todos[position] = newValue;
+//   displayTodos();
+// }
+//
+// function deleteTodo(position) {
+//   todos.splice(position, 1);
+//   displayTodos();
+// }
