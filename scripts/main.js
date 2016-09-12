84421609f3636e38fe9e77b9698c5b6428d234f5
Raleigh Green <raleighgreen@gmail.com>
2016-09-12 12:51:17 -0400

    Make deleteTodo() function

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,8 @@ function changeTodo(position, newValue) {
   todos[position] = newValue;
   displayTodos();
 }
+
+function deleteTodo(position) {
+  todos.splice(position, 1);
+  displayTodos();
+}
