f86f07fe9fa365d0b82810157a2f1eef1ba9f452
Raleigh Green <raleighgreen@gmail.com>
2016-09-16 16:21:40 -0400

    wrap up version 8

--- scripts/main.js
+++ scripts/main.js
@@ -62,9 +62,6 @@ var handlers = {
     displayTodos: function() {
         todoList.displayTodos();
     },
-    toggleAll: function() {
-        todoList.toggleAll();
-    },
     addTodo: function() {
         var addTodoTextInput = document.getElementById('addTodoTextInput');
         todoList.addTodo(addTodoTextInput.value);
@@ -86,5 +83,8 @@ var handlers = {
       var toggleCompletedPositionInput  = document.getElementById('toggleCompletedPositionInput');
       todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
       toggleCompletedPositionInput.value = '';
+    },
+    toggleAll: function() {
+        todoList.toggleAll();
     }
 };
