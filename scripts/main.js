5af746ef2fb3b4a2e14e592a1805a47d1437f910
Raleigh Green <raleighgreen@gmail.com>
2016-09-29 14:39:01 -0400

    create delete buttons

--- scripts/main.js
+++ scripts/main.js
@@ -90,5 +90,11 @@ var view = {
       todoLi.textContent = todoTextWithCompletion;
       todosUl.appendChild(todoLi);
     }
+  },
+  createDeleteButton: function() {
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButton';
+    return deleteButton;
   }
 };
